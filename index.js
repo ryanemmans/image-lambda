152ad58a917b4cb007f5d3645ed28ec2ebec5920
Ryan Emmans <ryanemmans@gmail.com>
2021-11-10 15:08:55 -0800

    Updates index.js, deletes images.json.

--- index.js
+++ index.js
@@ -2,11 +2,45 @@ const AWS = require('aws-sdk');
 let s3 = new AWS.S3();
 require('dotenv').config();
 
-exports.handler = async (event) => {
-  console.log(s3);
-  let bucket = event['Records'][0]['s3']['bucket'];
-  let object = event['Records'][0]['s3']['object'];
-  console.log(bucket, object);
+exports.handler = async(event) => {
+
+  const bucketName = event.Records[0].s3.bucket.name;
+  const fileName = event.Records[0].s3.object.key;
+  let fileSize = event.Records[0].s3.object.size;
+
+  console.log(bucketName, fileName, fileSize);
+
+  const params = {
+    Bucket: bucketName,
+    Key: 'images.json',
+  };
+
+  try {
+    const manifest = await s3.getObject(params).promise();
+
+    let manifestData = JSON.parse(manifest.Body.toString());
+
+    manifestData.push({
+      name: fileName,
+      size: fileSize,
+      type: 'image',
+    });
+    let manifestBody = JSON.stringify(manifestData);
+
+    const newManifest = await s3.putObject({ ...params, Body: manifestBody, ContentType: 'application/json' }).promise();
+
+  }
+  catch (e) {
+    console.log(e);
+    const newManifest = {
+      Bucket: bucketName,
+      Key: 'images.json',
+      Body: JSON.stringify([{ name: fileName, size: fileSize, type: 'image' }]),
+      ContentType: 'application/json',
+    };
+    const manifest = await s3.putObject(newManifest).promise();
+    console.log('JSON file created for bucket:', manifest);
+  }
 
   const response = {
     statusCode: 200,
